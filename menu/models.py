0c3ad9a9fc62269b21e8c8fdca4bcf92730c10f7
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-11 10:49:21 +0200

    added menu application

---
+++ menu/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_display_name(self):
+        return self.display_name
+
+class Item(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    item = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
