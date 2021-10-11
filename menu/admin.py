a8ca3b97d85995806852e86f7620ae6d040e3371
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-11 11:39:02 +0200

    customized admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Category, Item
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'item',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
 
-admin.site.register(Item)
-admin.site.register(Category)
+    ordering = ('item',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
