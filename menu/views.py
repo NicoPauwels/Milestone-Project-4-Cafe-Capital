83d215a6f8b7107cbe1965173e9f3247de1ec6c0
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-11 12:02:06 +0200

    added menu views and templates

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+def all_items(request):
+    """A view to return all items, including filtering"""
+
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+    
+    return render(request, 'menu/menu.html', context)
