929d3807271729a9d5394542a570625a36b712b8
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-11 10:45:31 +0200

    added sidebar nav

---
+++ static/js/corejs.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mysidenav").style.width = "250px";
+    document.getElementById("sidenavoverlay").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+    document.getElementById("sidenavoverlay").style.display = "none";
+  }
