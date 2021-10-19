2185a67054d5f42d95899e4d892e305569b1cc2a
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-19 18:17:00 +0200

    removed redundant lines

--- static/js/corejs.js
+++ static/js/corejs.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("mysidenav").style.width = "250px";
-    document.getElementById("sidenavoverlay").style.display = "block";
-  }
+  document.getElementById("mysidenav").style.width = "250px";
+  document.getElementById("sidenavoverlay").style.display = "block";
+}
   
-  function closeNav() {
-    document.getElementById("mysidenav").style.width = "0";
-    document.getElementById("sidenavoverlay").style.display = "none";
-  }
+function closeNav() {
+  document.getElementById("mysidenav").style.width = "0";
+  document.getElementById("sidenavoverlay").style.display = "none";
+}
+
+
