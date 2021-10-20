c35319687f5a7a5474bff7d59ec0531f8f540c51
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-20 16:49:25 +0200

    added tab scripts

--- static/js/corejs.js
+++ static/js/corejs.js
@@ -7,3 +7,13 @@ function closeNav() {
   document.getElementById("mysidenav").style.width = "0";
   document.getElementById("sidenavoverlay").style.display = "none";
 }
+
+function openTab() {
+  document.getElementById("tab").style.width = "320px";
+  document.getElementById("taboverlay").style.display = "block";
+}
+  
+function closeTab() {
+  document.getElementById("tab").style.width = "0";
+  document.getElementById("taboverlay").style.display = "none";
+}
