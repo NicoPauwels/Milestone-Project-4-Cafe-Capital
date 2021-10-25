29a57792877fa84842531f4ae753c9a713f449c8
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-25 14:11:09 +0200

    added remove from tab function

--- tab/views.py
+++ tab/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import redirect, reverse
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, reverse, HttpResponse
+from menu.views import all_items
 
 # Create your views here.
 
@@ -27,7 +29,23 @@ def adjust_tab(request, item_id):
     if quantity > 0:
         tab[item_id] = quantity
     else:
-        tab.pop[item_id]
+        tab.pop(item_id)
 
     request.session['tab'] = tab
-    return redirect(reverse('all_items'))
+    return redirect(reverse(all_items))
+
+
+def remove_from_tab(request, item_id):
+    """ Remove item from the tab """
+
+    try:
+        tab = request.session.get('tab', {})
+        print(tab)
+
+        tab.pop(item_id)
+
+        request.session['tab'] = tab
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
