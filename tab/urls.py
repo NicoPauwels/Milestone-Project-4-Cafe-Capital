a6a88babe27cd4b2d2a037b92b7a842a03a8c773
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-25 13:49:32 +0200

    added remove item url

--- tab/urls.py
+++ tab/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add/<item_id>', views.add_to_tab, name='add_to_tab'),
-    path('adjust/<item_id>', views.adjust_tab, name='adjust_tab'),
+    path('adjust/<item_id>/', views.adjust_tab, name='adjust_tab'),
+    path('remove/<item_id>/', views.remove_from_tab, name='remove_from_tab'),
 ]
