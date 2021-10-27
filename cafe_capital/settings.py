450e0af01d43c9a1f68ad6c1fe2ae000e597a3ae
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-27 15:51:16 +0200

    changed name of context processor in settings

--- cafe_capital/settings.py
+++ cafe_capital/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'tab.context.show_tab',
+                'tab.context.tab_content',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -169,3 +169,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+
+STRIPE_CURRENCY = 'EUR'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
