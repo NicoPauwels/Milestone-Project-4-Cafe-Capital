9790c008d3296b8d47ec639d6979c7a88d26b1ef
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-27 11:53:16 +0200

    added checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    tab = request.session.get('tab', {})
+    if not tab:
+        messages.error(request, "There's nothing on your tab at the moment")
+        return redirect(reverse('all_items'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
