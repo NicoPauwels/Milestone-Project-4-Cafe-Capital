2116076ece6f47028cead3862d1021a02032ba0a
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-29 18:48:22 +0200

    added user profile field to order admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -13,7 +13,8 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = (
-        'order_number', 
+        'order_number',
+        'user_profile', 
         'date', 
         'order_total',
         'original_tab',
@@ -22,6 +23,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     fields = (
         'order_number',
+        'user_profile',
         'date',
         'full_name',
         'email', 
