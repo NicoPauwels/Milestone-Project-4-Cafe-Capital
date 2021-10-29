4828d130f806fcfe04aceeb190f4bfe7a453c55a
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-29 14:23:10 +0200

    added profile form, views and updated templates

---
+++ profiles/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):    
+        """ Adds placeholders and classes to the form, removes auto labels and set focus on first field """
+        
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'postcode': 'Postcode',
+            'town_or_city': 'Town Or City',
+        }
+
+        self.fields['phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
