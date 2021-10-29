2624052798015ca93b1e06bed447149320c9f242
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-29 18:58:30 +0200

    added profile view and template

--- profiles/views.py
+++ profiles/views.py
@@ -8,7 +8,7 @@ from .forms import UserProfileForm
 def profile(request):
     """ Display the user profile """
     profile = get_object_or_404(UserProfile, user=request.user)
-    
+        
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid:
@@ -17,12 +17,11 @@ def profile(request):
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
-
+    
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True
     }
 
     return render(request, template, context)
