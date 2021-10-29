40d583c5c0edc9dae52d00d16b0da82abfeb817a
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-29 22:48:57 +0200

    added messages to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -14,8 +14,10 @@ def profile(request):
         if form.is_valid:
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Profile was not updated')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     
     template = 'profiles/profile.html'
