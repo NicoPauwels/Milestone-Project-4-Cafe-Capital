c21034f6b9c3ae09340719365811a3c24847b0c7
NicoPauwels <nico_pauwels@hotmail.com>
2021-10-30 03:55:35 +0200

    added media context processor

--- cafe_capital/settings.py
+++ cafe_capital/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'tab.context.tab_content',
             ],
             'builtins': [
@@ -222,3 +223,5 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
